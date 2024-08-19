dfd5594324f5ff04840c01ad89e125c210d747ec
pala26 <tarunpala.revind@gmail.com>
2024-08-19 14:57:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
+
+
 #!/bin/bash
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 # Check which branch is being built
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
@@ -6,29 +10,25 @@ if [[ $GIT_BRANCH == "origin/dev" ]]; then
     chmod +x build.sh
     ./build.sh
 
-    # Log in to Docker Hub
-    docker login -u pala29 -p $DOCKER_PASSWORD
-
     # Tag the Docker image for the dev environment
-    docker tag reactapp pala29/dev
+    docker tag reactapp $DOCKER_USERNAME/dev
 
     # Push the Docker image to the dev repository
-    docker push pala29/dev
+    docker push $DOCKER_USERNAME/dev
 
 elif [[ $GIT_BRANCH == "origin/main" ]]; then
     chmod +x build.sh
     ./build.sh
 
-    # Log in to Docker Hub
-    docker login -u pala29 -p $DOCKER_PASSWORD
-
     # Tag the Docker image for the prod environment
-    docker tag reactapp pala29/prod
+    docker tag reactapp $DOCKER_USERNAME/prod
 
     # Push the Docker image to the prod repository
-    docker push pala29/prod
+    docker push $DOCKER_USERNAME/prod
 else 
-    echo "Branch-not recognized. No build or push will occur."
+    echo "Branch not recognized. No build or push will occur."
     exit 1
 fi
 
+docker-compose down || true
+docker-compose up -d
